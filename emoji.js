4225001f18136fdfb9e89842cde44fb3c00993bb
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2022-12-13 22:35:49 -0600

    javascript part

---
+++ emoji.js
@@ -0,0 +1,36 @@
+let images = ['😁','🤑','🐱‍🐉','👿',];
+
+
+document.addEventListener('mousemove', function(e){
+    let body = document.querySelector('body');
+    let emoji = document.createElement('span');
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    emoji.style.left = x+'px';
+    emoji.style.top = y+'px';
+
+    let icon = images[Math.floor(Math.random() * 
+        images.length)];
+        emoji.innerText = icon;
+
+        let size = Math.random()  * 50;
+        emoji.style.fontSize = 5 +size+'px';
+
+        // random value
+
+        let max = 0;
+        let min = 200;
+        let randomValue = Math.floor((Math.random() * 
+        ((max + 1) - min)) + min);
+
+        emoji.style.transform = 'translateX('+ randomValue +'px)';
+
+
+    body.appendChild(emoji);
+
+
+    setTimeout(function(){
+        emoji.remove();
+    },1000)
+})
